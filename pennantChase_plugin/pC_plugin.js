49d287a33223389a1efadb6d2b346af7979691ff
chaaad <chaaad@yahoo.com>
2026-02-18 19:39:27 -0500

    bugfix pC

--- pennantChase_plugin/pC_plugin.js
+++ pennantChase_plugin/pC_plugin.js
@@ -20,30 +20,25 @@ tmx.hookers["init"]= function() { //hook fn from main page
 
 
 tmx.hookers["extAddAlarm"]= function(spl_ar) { //hook fn from main page
-//external|WED 5/5 12:34pm|PC|New York Yankees|K|123-1 //to test
-//0        1               2  3                    4 5
-
-//NEW
+  if (spl_ar[2] != "PC") return; //-->
 /*
+// external|2/19 6:39pm|PC|THU|Hello Kitty|K|1119-12
 test "add" in console:
 TMX.data_save("externalAlarms", "external|5/5 12:34pm|PC|WED|New York Yankees|K|123-1");
-                                 0        1           2  3   4                    5 6
+idx:                             0        1           2  3   4                    5 6
 */
-  if (spl_ar[2] != "PC") return; //-->
-
   var k_str= spl_ar[5] ? " (K)" : ""; //keeper league
   return `${spl_ar[3]} <br><i>${spl_ar[4]}</i>${k_str} <i class="PC_tid" title="?">${spl_ar[6]}</i>`;
 }; //tmx.hookers["extAddAlarm"]()
 
 
 tmx.hookers["extDeleteAlarm"]= function(spl_ar) { //hook fn from main page
+  if (spl_ar[2] != "PC") return; //-->
 /*
 test "delete" in console:
 TMX.data_save("externalAlarms", "externalDelete|123-1|PC");
-                                 0              1     2
+idx:                             0              1     2
 */
-  if (spl_ar[2] != "PC") return; //-->
-
   var tid= spl_ar[1];
   var delTid_EL= [...tmx.alarmsCont_EL.querySelectorAll("i.PC_tid")]
     .find(EL => EL.textContent == tid)
